55d8b882663d6a8fe2f6dd3c2f3401add40b5fb2
huyjohnson <66340919+huyjohnson@users.noreply.github.com>
2020-06-07 00:52:47 -0400

    Create ExhaustEnumeration.py

---
+++ ExhaustEnumeration.py
@@ -0,0 +1,23 @@
+# - Finger Exercise - Chapter 3.1
+num = int(input('Please enter an integer: '))
+root = 0
+pwr = 2
+endbound = 6
+while pwr < endbound and root**pwr != abs(num):
+    while root < abs(num):
+        root += 1
+        if root**pwr == abs(num):
+            if num > 0:
+                break
+            elif num < 0 and pwr % 2 == 1:
+                root = -root
+                break
+    if root**pwr == num:
+        print('root = {}, pwr = {}'.format(root, pwr))
+        break
+    elif root == abs(num):
+        root = 0
+    pwr += 1
+    
+if pwr == endbound:
+    print('No existing set of integers.')
